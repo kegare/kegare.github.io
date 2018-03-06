b59f896da666a41f9da4828e778330da24f96e83
kegare <kegare0127@gmail.com>
2018-03-07 08:05:36 +0900

    Update

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,4 +1,3 @@
-//jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -7,7 +6,6 @@ $(window).scroll(function() {
     }
 });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
